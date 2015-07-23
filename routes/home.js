d9d37f723e05f10fc8fe1c0caaa70082b2ff16e1
Vincent Tertre <vincent.tertre@gmail.com>
2015-07-23 03:06:35 +0200

    Dummy footer

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,7 @@ var i18n = require("i18next");
 
 exports.index = function (request, response) {
   response.render("index", {
-    title: i18n.t("app.global.title")
+    title: i18n.t("app.global.title"),
+    twitter_account: i18n.t("app.global.footer.twitter_account")
   });
 };
