88e325fe5cd5f40937c2953454bc8ffbe4414bba
Vincent Tertre <vincent.tertre@gmail.com>
2015-07-29 23:04:35 +0200

    Minimum of SEO

--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,7 @@ var i18n = require("i18next");
 exports.index = function (request, response) {
   response.render("index", {
     title: i18n.t("app.global.title"),
+    description: i18n.t("app.global.description"),
     twitter_account: i18n.t("app.global.footer.twitter_account")
   });
 };
