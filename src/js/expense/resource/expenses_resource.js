31ac7b54ad92c8e9d484b693bff64e442e7e34ef
Vincent Tertre <vincent.tertre@gmail.com>
2015-10-22 00:00:37 +0200

    Improve readability

--- src/js/expense/resource/expenses_resource.js
+++ src/js/expense/resource/expenses_resource.js
@@ -3,27 +3,37 @@
 /* @ngInject */
 function ExpensesResource($http) {
   return {
-    metadata: function (eventId) {
-      return $http.get("/api/events/" + eventId + "/expenses/meta").then(forwardResponseData);
-    },
-    fetchWithCount: function (eventId, skip, limit) {
-      return this.fetch(eventId, skip, limit, true);
-    },
-    fetch: function (eventId, skip, limit, withCount) {
-      withCount = withCount || false;
-      var url = "/api/events/" + eventId + "/expenses?skip=" + skip + "&limit=" + limit;
-      return $http.get(url).then(function (response) {
-        return withCount ? {expenseCount: response.data.expenseCount, expenses: response.data.expenses} :
-          response.data.expenses;
-      });
-    },
-    add: function (eventId, expense) {
-      return $http.post("/api/events/" + eventId + "/expenses", expense).then(forwardResponseData);
-    },
-    delete: function (eventId, expenseId) {
-      return $http.delete("/api/events/" + eventId + "/expenses/" + expenseId);
-    }
+    add: add,
+    delete: deleteExpense,
+    fetch: fetch,
+    fetchWithCount: fetchWithCount,
+    metadata: metadata
   };
+
+  function add(eventId, expense) {
+    return $http.post("/api/events/" + eventId + "/expenses", expense).then(forwardResponseData);
+  }
+
+  function deleteExpense(eventId, expenseId) {
+    return $http.delete("/api/events/" + eventId + "/expenses/" + expenseId);
+  }
+
+  function fetch(eventId, skip, limit, withCount) {
+    withCount = withCount || false;
+    var url = "/api/events/" + eventId + "/expenses?skip=" + skip + "&limit=" + limit;
+    return $http.get(url).then(function (response) {
+      return withCount ? {expenseCount: response.data.expenseCount, expenses: response.data.expenses} :
+        response.data.expenses;
+    });
+  }
+
+  function fetchWithCount(eventId, skip, limit) {
+    return fetch(eventId, skip, limit, true);
+  }
+
+  function metadata(eventId) {
+    return $http.get("/api/events/" + eventId + "/expenses/meta").then(forwardResponseData);
+  }
 
   function forwardResponseData(response) {
     return response.data;
