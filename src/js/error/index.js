f5c50442e697999d863e08bd824b394ffaab7e85
Vincent Tertre <vincent.tertre@gmail.com>
2015-10-31 15:09:36 +0100

    Drop 404 error interceptor

--- src/js/error/index.js
+++ src/js/error/index.js
@@ -6,13 +6,11 @@ var configureModuleRouting = require("./module_routing");
 var errorModule = angular.module("app.error", []);
 
 errorModule
-  .factory("error404InterceptorService", require("./service/error_404_interceptor_service"))
   .factory("defaultErrorInterceptorService", require("./service/default_error_interceptor_service"));
 
 /* @ngInject */
 errorModule
   .config(function ($httpProvider) {
-    $httpProvider.interceptors.push("error404InterceptorService");
     $httpProvider.interceptors.push("defaultErrorInterceptorService");
   });
 
