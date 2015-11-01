14f05c83e6babed8b11bacc6a0e9ee4f29eca22f
Vincent Tertre <vincent.tertre@gmail.com>
2015-11-01 17:26:13 +0100

    Refactor: cleanup translation

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,8 @@ var i18n = require("i18next");
 
 exports.index = function (request, response) {
   response.render("index", {
-    title: i18n.t("app.global.title"),
-    description: i18n.t("app.global.description"),
-    twitter_account: i18n.t("app.global.footer.twitter_account")
+    title: i18n.t("app.title"),
+    description: i18n.t("app.description"),
+    twitter_account: i18n.t("app.twitter_account")
   });
 };
