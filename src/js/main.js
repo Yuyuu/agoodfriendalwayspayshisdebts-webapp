83fbe3f884b6928a98a0c23217b0f19a66994d55
Vincent Tertre <vtertre@gpartner.eu>
2016-02-10 12:22:08 +0100

    add font-awesome

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import uirouter from 'angular-ui-router';
 import event from './event';
 import 'angular-bootstrap';
 import 'jquery';
+import '../../node_modules/font-awesome/fonts/fontawesome-webfont.woff';
 import '../less/style.less';
 
 angular.module('app', [event, uirouter, 'ui.bootstrap']);
